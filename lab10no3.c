dd539be5f9d8997a916f905d57409de569de2f85
6804062610215 <s6804062610215@email.kmutnb.ac.th>
2026-03-13 06:49:05 +0700

    Update lab10no3.c

--- lab10no3.c
+++ lab10no3.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct studentRecord {
-	char name[64];
-	int age;
-};
-//functions prototype
-void displayRecord(struct studentRecord student);
-void addAge(struct studentRecord *student, int age);
+struct Books{
+    int BookID;
+    char BookTitle[50];
+} ComputerBook;
 
-int main () {
-	struct studentRecord student[5];
-	char tempName[64]; 
-	int i;
-	
-	
-	for(i = 0; i < 5; i++) {
-		sprintf(tempName, "Student%d", i);
-		strcpy(student[i].name, tempName);
-		student[i].age = i+15;
-	}
-	
-	for(i = 0; i < 5; i++){
-		addAge(&student[i], 10);
-	}
-	
-	for(i = 0; i < 5; i++){
-		displayRecord(student[i]);
-	}
+void DisplayData(struct Books CBook);
 
-	return 0;
+int main() {
+    printf("Enter Book ID : ");
+    scanf("%d", &ComputerBook.BookID);
+
+    printf("Enter Book Title : ");
+    scanf("%s", ComputerBook.BookTitle);
+
+    DisplayData(ComputerBook);
+
+    return 0;
 }
 
-
-void addAge(struct studentRecord *student, int age){
-	student->age = student->age + age;
+void DisplayData(struct Books CBook){
+    printf("Book ID : %d \n", CBook.BookID);
+    printf("Book Title : %s \n", CBook.BookTitle);
 }
 
-void displayRecord(struct studentRecord student){
-	printf("Name : %s, Age : %d\n", student.name, student.age);
-}
-
+// void DisplayData(struct Books CBook) คือ ฟังก์ชันไว้แสดงข้อมูล Book ID และ Book Title
+// พารามิเตอร์ที่รับเข้ามา คือ CBook ของ struct
